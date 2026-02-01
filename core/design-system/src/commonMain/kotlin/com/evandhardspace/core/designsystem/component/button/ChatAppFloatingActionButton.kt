e27b86576fd950da90d2b8ed3d5cb6439c4b6e72
Ivan Kravchenko <ivan.kravchenko217@gmail.com>
2026-02-01 01:39:12 +0100

    Fixed imports

--- core/design-system/src/commonMain/kotlin/com/evandhardspace/core/designsystem/component/button/ChatAppFloatingActionButton.kt
+++ core/design-system/src/commonMain/kotlin/com/evandhardspace/core/designsystem/component/button/ChatAppFloatingActionButton.kt
@@ -1,6 +1,5 @@
 package com.evandhardspace.core.designsystem.component.button
 
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -8,7 +7,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.evandhardspace.core.designsystem.annotations.ThemedPreview
 import com.evandhardspace.core.designsystem.theme.ChatAppPreview
 
