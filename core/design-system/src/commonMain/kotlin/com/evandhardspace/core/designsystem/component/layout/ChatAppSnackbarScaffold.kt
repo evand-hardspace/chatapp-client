f3a8eb183d057061cddd96a5607410e30d979dcd
Ivan Kravchenko <ivan.kravchenko217@gmail.com>
2026-02-03 22:38:03 +0100

    Added ChatAppSnackbarScaffold

---
+++ core/design-system/src/commonMain/kotlin/com/evandhardspace/core/designsystem/component/layout/ChatAppSnackbarScaffold.kt
@@ -0,0 +1,43 @@
+package com.evandhardspace.core.designsystem.component.layout
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.union
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.evandhardspace.core.designsystem.theme.paddings
+
+@Composable
+fun ChatAppSnackbarScaffold(
+    snackbarHostState: SnackbarHostState,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    Scaffold(
+        modifier = modifier,
+        contentWindowInsets = WindowInsets.statusBars
+            .union(WindowInsets.displayCutout)
+            .union(WindowInsets.ime),
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackbarHostState,
+                modifier = Modifier
+                    .padding(bottom = MaterialTheme.paddings.fiveQuarters),
+            )
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            content()
+        }
+    }
+}
