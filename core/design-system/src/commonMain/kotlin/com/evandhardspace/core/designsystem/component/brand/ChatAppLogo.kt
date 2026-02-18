1ca5a09ac9cb6d36d06e29aacf108b23959bdf87
Ivan Kravchenko <ivan.kravchenko217@gmail.com>
2026-02-18 13:54:45 +0100

    Added android splash screen and fix icon

--- core/design-system/src/commonMain/kotlin/com/evandhardspace/core/designsystem/component/brand/ChatAppLogo.kt
+++ core/design-system/src/commonMain/kotlin/com/evandhardspace/core/designsystem/component/brand/ChatAppLogo.kt
@@ -1,13 +1,11 @@
 package com.evandhardspace.core.designsystem.component.brand
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import chatapp.core.design_system.generated.resources.Res
 import chatapp.core.design_system.generated.resources.a11y_chatapp_logo
-import chatapp.core.design_system.generated.resources.logo_chatapp_dark
-import chatapp.core.design_system.generated.resources.logo_chatapp_light
+import chatapp.core.design_system.generated.resources.logo_chatapp
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.resources.vectorResource
 
@@ -16,10 +14,7 @@ fun ChatAppBrandLogo(
     modifier: Modifier = Modifier,
 ) {
     Image(
-        imageVector = vectorResource(
-            if (isSystemInDarkTheme()) Res.drawable.logo_chatapp_dark
-            else Res.drawable.logo_chatapp_light,
-        ),
+        imageVector = vectorResource(Res.drawable.logo_chatapp),
         contentDescription = stringResource(Res.string.a11y_chatapp_logo),
         modifier = modifier,
     )
